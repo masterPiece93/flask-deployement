63d74b13d8ff45271d27beea7c0dcfccac77d726
Ankit Yadav <42907602+masterPiece93@users.noreply.github.com>
2024-12-19 17:41:00 +0530

    added new template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return """
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <h1 style="
         text-align: center;
         background: #000000;
@@ -13,8 +14,20 @@ def home():
         "
     >
     THE ANKIT </h1>
+
+    <div style="text-align: center;">
+    <a href="/ankit-loves-shalu">
+    <i class="fa fa-heart" style="font-size:48px;color:red">
+    </i>
+    </a>
+    </div>
     """
 
 @app.route('/health')
 def health():
     return {"healthy": True}
+
+@app.route('/ankit-loves-shalu')
+def message_1():
+    return render_template('shalu.html')
+
