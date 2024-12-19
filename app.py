d96b79d0c957e00477aeefbafca2a2b80d48b244
Ankit Yadav <42907602+masterPiece93@users.noreply.github.com>
2024-12-19 16:40:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
     return """
-    <h1>HOME</h1>
+    <h1 style="
+        text-align: center;
+        background: #000000;
+        color: #FFD700;
+        border: 0.1em solid gold;
+        "
+    >
+    THE ANKIT </h1>
     """
+
+@app.route('/health')
+def health():
+    return {"healthy": True}
